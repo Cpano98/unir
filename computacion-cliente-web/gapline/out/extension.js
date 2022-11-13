63eff550ae602c60bd4ce189f7b24cc311f9ee17
Carlos Pano <carlos.pano@oracle.com>
2022-11-13 14:26:49 -0600

    Update

--- computacion-cliente-web/gapline/out/extension.js
+++ computacion-cliente-web/gapline/out/extension.js
@@ -2,37 +2,37 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require("vscode");
+const vscode = require("vscode"); // Import the module and reference it with the alias vscode in your code below
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 function activate(context) {
+    // Your extension is activated the very first time the command is executed
     let disposable = vscode.commands.registerCommand("extension.gapline", () => {
-        var editor = vscode.window.activeTextEditor;
+        var editor = vscode.window.activeTextEditor; //Inicializa el editor de texto
         if (!editor) {
             return;
         }
-        var selection = editor.selection;
-        var text = editor.document.getText(selection);
+        var selection = editor.selection; //Estado de nuestro usuario: selección. El programa recorrerá solo el texto seleccionado
+        var text = editor.document.getText(selection); //Guarda el texto seleccionado por el usuario
         vscode.window.showInputBox({ prompt: "Lineas?" }).then((value) => {
-            let numberOfLines = +value;
-            var textInChunks = [];
+            //Crea input para usuario de número de líneas a saltar. Después de enter, la función continúa con el programa
+            let numberOfLines = +value; //Genera nuestra variable con el input del usuario.
+            var textInChunks = []; //Genera array/stack deL CONTENIDO (vacío)
             text.split("\n").forEach((currentLine, lineIndex) => {
-                textInChunks.push(currentLine);
+                //Llama a la función "split" para detectar cada salto de línea y recorre el string completo con un ForEach.
+                textInChunks.push(currentLine); //Agrega a nuestro stack vacío el contenido de cada línea.
                 if ((lineIndex + 1) % numberOfLines === 0)
-                    textInChunks.push("");
+                    textInChunks.push(""); //Llama nuestra variable "numberOfLines" para hacer el salto correspondiente
             });
-            text = textInChunks.join("\n");
+            text = textInChunks.join("\n"); //Junta nuestro nuevo array con salto de línea "\n"
             editor.edit((editBuilder) => {
                 var range = new vscode.Range(selection.start.line, 0, selection.end.line, editor.document.lineAt(selection.end.line).text.length);
                 editBuilder.replace(range, text);
             });
         });
     });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable); //Variable de entorno
 }
 exports.activate = activate;
-//Extension in deactivated: Calls this function in case the ext. is no longer required by the user
-function deactivate() { }
+function deactivate() { } //Extension in deactivated: Calls this function in case the ext. is no longer required by the user
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- computacion-cliente-web/gapline/src/extension.ts
+++ computacion-cliente-web/gapline/src/extension.ts
@@ -1,30 +1,31 @@
 "use strict"; //Deactivated on the tsconfig.json file.
 
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import * as vscode from "vscode"; // Import the module and reference it with the alias vscode in your code below
 
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Your extension is activated the very first time the command is executed
   let disposable = vscode.commands.registerCommand("extension.gapline", () => {
-    var editor = vscode.window.activeTextEditor;
+    var editor = vscode.window.activeTextEditor; //Inicializa el editor de texto
     if (!editor) {
       return;
     }
-    var selection = editor.selection;
-    var text = editor.document.getText(selection);
+    var selection = editor.selection; //Estado de nuestro usuario: selección. El programa recorrerá solo el texto seleccionado
+    var text = editor.document.getText(selection); //Guarda el texto seleccionado por el usuario
 
     vscode.window.showInputBox({ prompt: "Lineas?" }).then((value) => {
-      let numberOfLines = +value;
-      var textInChunks: Array<string> = [];
+      //Crea input para usuario de número de líneas a saltar. Después de enter, la función continúa con el programa
+      let numberOfLines = +value; //Genera nuestra variable con el input del usuario.
+      var textInChunks: Array<string> = []; //Genera array/stack deL CONTENIDO (vacío)
 
       text.split("\n").forEach((currentLine: string, lineIndex) => {
-        textInChunks.push(currentLine);
-        if ((lineIndex + 1) % numberOfLines === 0) textInChunks.push("");
-	});
+        //Llama a la función "split" para detectar cada salto de línea y recorre el string completo con un ForEach.
+        textInChunks.push(currentLine); //Agrega a nuestro stack vacío el contenido de cada línea.
+        if ((lineIndex + 1) % numberOfLines === 0) textInChunks.push(""); //Llama nuestra variable "numberOfLines" para hacer el salto correspondiente
+      });
 
-      text = textInChunks.join("\n");
+      text = textInChunks.join("\n"); //Junta nuestro nuevo array con salto de línea "\n"
       editor.edit((editBuilder) => {
         var range = new vscode.Range(
           selection.start.line,
@@ -36,8 +37,7 @@ export function activate(context: vscode.ExtensionContext) {
       });
     });
   });
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable); //Variable de entorno
 }
 
-//Extension in deactivated: Calls this function in case the ext. is no longer required by the user
-export function deactivate() {}
+export function deactivate() {} //Extension in deactivated: Calls this function in case the ext. is no longer required by the user
